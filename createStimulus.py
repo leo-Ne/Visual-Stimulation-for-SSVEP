d5f243951e5d44633649f714502e1e0c88885925
leo-Ne <xieshiyao_tju@163.com>
2020-12-29 15:17:07 +0800

    A module to generate various stimulus.

---
+++ createStimulus.py
@@ -0,0 +1,17 @@
+#! /use/bin/env python
+''' **************************************
+# Author       :leo-Ne
+# Last modified:2020-12-29 14:45
+# Email        : leo@email.com
+# Filename     :createStimulus.py
+# Description  : 
+**************************************'''
+
+import cv2 as cv
+import numpy as np 
+import time as time 
+
+class Stimulus:
+
+    def __init__(self, stimulusNumber=0):
+        
